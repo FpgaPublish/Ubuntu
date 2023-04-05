66e4df73298f32cd90d8c491adc2a4938fa1bc3d
fpgaPublish <fu3038605879@163.com>
2023-04-05 16:40:52 +0800

    petalinux的基本配置

---
+++ imag_eye_ps_demo/linux_base.apps/hello/main.c
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Fpga Publish
+// Engineer: F 
+// 
+// Create Date: 2023/04/02 12:11:28
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: ZYNQ7010 & XAUG
+// Tool Versions: vitis 2021.1
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision: 0.01 
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+int main(void)
+{
+    printf("hello world\r\n");
+    return 0;
+}
